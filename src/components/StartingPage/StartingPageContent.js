23022cb99feaf9e8807b313edf53e47cfb1d0883
Aruns96 <arun509577@gmail.com>
2024-07-01 11:43:05 +0530

    Auto logout

--- src/components/StartingPage/StartingPageContent.js
+++ src/components/StartingPage/StartingPageContent.js
@@ -1,6 +1,13 @@
+
 import classes from './StartingPageContent.module.css';
+import { useEffect } from 'react';
 
 const StartingPageContent = () => {
+  
+  
+  useEffect(()=>{
+    setTimeout(()=>{localStorage.removeItem("token")},300000)
+})
   return (
     <section className={classes.starting}>
       <h1>Welcome on Board!</h1>
